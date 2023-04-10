ac29b80b3972b5265bad316d31faebcf14941d56
Dmitrii Rachkov <Dmitrii_Rachkov@epam.com>
2023-04-10 10:50:37 +0300

    Autocompleted fix

--- firstDB_4_insert_many.py
+++ firstDB_4_insert_many.py
@@ -1,8 +1,12 @@
 import psycopg2
 
+import psycopg2.extras  # Конструкция для автозаполнения кода
+
+
 conn = psycopg2.connect(dbname="metanit", user="postgres", password="12PostgreSQL05",
                         host="localhost", port="5432")
-cursor = conn.cursor()
+# Чтобы включить автозаполнение кода нужна следующая конструкция:
+cursor = conn.cursor()  # type: psycopg2.extras.DictCursor
 
 # Метод executemany() позволяет вставить набор строк:
 people = [("Sam", 28), ("Alice", 33), ("Kate", 25)]
@@ -14,4 +18,6 @@ print("Data added")
 
 cursor.close()
 conn.close()
+
+
 
